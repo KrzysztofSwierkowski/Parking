133b130f5e2ff0f40959eae640c55597bd6ba1cd
Swierkowski.K@gmail.com <Krzysiu3285>
2021-11-22 01:36:39 +0100

    Initial commit

---
+++ src/test/java/pl/parking/Parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.parking.Parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
