3c82943e3ad90c665a99100fd65cac18804b984b
Swierkowski.K@gmail.com <Krzysiu3285>
2021-11-25 23:18:25 +0100

    configuration

--- src/main/java/pl/parking/Parking/controller/MainController.java
+++ src/main/java/pl/parking/Parking/controller/MainController.java
@@ -2,15 +2,13 @@ package pl.parking.Parking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String page(){
-        return "PARKING";
+    public String parking(){
+        return "home";
     }
 
 
--- src/test/java/pl/parking/Parking/ParkingApplicationTests.java
+++ src/test/java/pl/parking/Parking/ParkingApplicationTests.java
@@ -1,13 +1,34 @@
 package pl.parking.Parking;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import pl.parking.Parking.controller.MainController;
 
-@SpringBootTest
-class ParkingApplicationTests {
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+@EnableWebMvc
 
+public class ParkingApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
-	void contextLoads() {
+	public void testParkingPage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("home"))
+				.andExpect(content().string(
+						containsString("Aplikacja Parking")));
+
 	}
 
 }
